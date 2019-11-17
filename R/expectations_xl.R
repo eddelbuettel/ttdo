bcfec0b08e8b8eff4797ca15f2eb1cdfa2750b6c
Alton Barbehenn <altonbarbehenn@gmail.com>
2019-11-17 15:12:46 -0600

    Adding xl extensions of the rest of the tinytest tests via templating (similar to checkmate.tinytest)

--- R/expectations_xl.R
+++ R/expectations_xl.R
@@ -1,6 +1,6 @@
 #' Extensions of equality tests for tinytest 
 #' 
-#' Building on the tinytest functions for testing equalty with optional enhanced object 
+#' Building on the tinytest functions for testing equality with optional enhanced object 
 #' diffing and additional test feedback through addtional attributes. 
 #' 
 #' While tinytest does now support the passing of additional information with the info
@@ -81,3 +81,81 @@ expect_equivalent_xl <- function(current, target, useDiffObj = TRUE,
     # Append all extra parameters as test attributes 
     structure(tt_res, ...)
 }
+
+
+#' Extensions of boolean and messaging tests from tinytest 
+#' 
+#' Building on the tinytest functions for testing boolean values with additional test 
+#' feedback through attributes. 
+#' 
+#' While tinytest does now support the passing of additional information with the info
+#' field in its tests, they are not yet supported in the \code{as.data.frame.tinytests}
+#' method.
+#'
+#' @param current \code{[R object or expression]} Outcome or expression under scrutiny.
+#' @param info scalar. Optional user-defined message. Must be a single character string. 
+#'     Multiline comments may be separated by "\\n".
+#' @param ... Passed to \code{all.equal} and returned as a test attribute
+#'
+#' @return A \code{\link{tinytest}} object. A tinytest object is a
+#' \code{logical} with attributes holding information about the
+#' test that was run
+#'
+
+#' @examples
+#' library(tinytest)
+#' using(ttdo)
+#' expect_true_xl(TRUE, score = 3) # TRUE
+#' expect_true_xl(FALSE, name = "check 1-1==2", score = 1, totalpts = 2) # FALSE
+#' 
+#' @name ttdo_boolean_and_message_tests
+NULL
+
+
+template = '{
+    tt_res <- %2$s(%3$s)
+    structure(tt_res, ...)
+}'
+
+## Borrowed heavily from checkmate.tinytest
+supersize <- function(tt.fun, env = parent.frame()) {
+    fun.name = if (!is.character(tt.fun)) deparse(substitute(tt.fun)) else tt.fun
+    tt.fun = match.fun(tt.fun)
+    fun.args = formals(args(tt.fun))
+    x.name = names(fun.args[1L])
+    body = sprintf(template, x.name, fun.name, paste0(names(fun.args), collapse = ", "))
+
+    # Append the ... operator to formals
+    new.args <- do.call(alist, c(fun.args, alist(... =)))
+
+    new.fun = function() TRUE
+    formals(new.fun) = new.args
+    body(new.fun) = parse(text = body)
+    environment(new.fun) = env
+
+    return(new.fun)
+}
+
+
+#' @rdname ttdo_boolean_and_message_tests
+expect_true_xl <- supersize(tinytest::expect_true)
+
+#' @rdname ttdo_boolean_and_message_tests
+expect_false_xl <- supersize(tinytest::expect_false)
+
+#' @rdname ttdo_boolean_and_message_tests
+expect_null_xl <- supersize(tinytest::expect_null)
+
+#' @rdname ttdo_boolean_and_message_tests
+#' @param quiet \code{[logical]} suppress output printed by the current expression (see examples)
+expect_silent_xl <- supersize(tinytest::expect_silent)
+
+#' @rdname ttdo_boolean_and_message_tests
+#' @param pattern \code{[character]} A regular expression to match the message.
+expect_error_xl <- supersize(tinytest::expect_error)
+
+#' @rdname ttdo_boolean_and_message_tests
+expect_warning_xl <- supersize(tinytest::expect_warning)
+
+#' @rdname ttdo_boolean_and_message_tests
+expect_message_xl <- supersize(tinytest::expect_message)
